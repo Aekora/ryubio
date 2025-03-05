6b04377e16c9b5220b939361092e778d405eaaf3
shay <sityounn@gmail.com>
2025-03-05 17:49:42 +0000

    Add files via upload

---
+++ effects2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgSrc = "https://discord.c99.nl/widget/theme-4/259110516922777600.png";
+    const imgElement = document.getElementById('rpc');
+    imgElement.src = imgSrc;
+});
