3c1ceabfeb3c575dc66570146826cc35f2fdc6bd
shay <sityounn@gmail.com>
2025-04-26 23:29:15 +0100

    Delete effects2.js

--- effects2.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const imgSrc = "https://discord.c99.nl/widget/theme-4/259110516922777600.png";
-    const imgElement = document.getElementById('rpc');
-    imgElement.src = imgSrc;
-});
